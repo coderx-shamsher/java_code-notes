998d9f3f92d0f78b7793a8af61a5ca86da1f3714
coderx-shamsher <shamsher000789@gmail.com>
2026-02-17 22:55:05 +0530

    day one patterns printing in java

---
+++ patterns/src/Pattrensecnd.java
@@ -0,0 +1,10 @@
+public class Pattrensecnd {
+  public static void main(){
+      System.out.println("* * * * * *");
+      System.out.println("* * * * * ");
+      System.out.println("* * * * ");
+      System.out.println("* * * ");
+      System.out.println("* * ");
+      System.out.println("* ");
+  }
+}
