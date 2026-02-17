51b52a1017fa4fb25fc8658bdec85cb551138b92
coderx-shamsher <shamsher000789@gmail.com>
2026-02-17 22:57:16 +0530

    day one variables numeric type covered only

---
+++ variables_and_data_types/Variables.java
@@ -0,0 +1,33 @@
+
+public class Variables {
+    public static void main(){
+        // a variables is used to store some data or values in programming
+        // variables are the memory location of a data that stored inside the system
+        /*
+        * syntax of variable
+        *
+        *  data_type variable_name = value
+        *
+        * */
+
+        // initialize variable using the int keyword
+        // set the variable name assignment operator and then the value you want to store inside the variable
+        int a = 100;
+        System.out.println(a);  // printing the value of variable using this function
+       byte b = 1;
+       short c = 2 ;
+       long l = 1000000290;
+        System.out.println(b);
+
+        char ch = 'a';
+
+        // declaration of variable
+        int dec_num ;
+        dec_num = 2000;
+        System.out.println(dec_num);
+        dec_num = 3222;    // re assign a value to the variable
+        System.out.println(dec_num);
+        dec_num = dec_num + 222 ;  // update the value
+        System.out.println(dec_num);
+    }
+}
